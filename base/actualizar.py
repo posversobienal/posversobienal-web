4cd5bb15ade4114c818ff576bea57a8cfccc32db
caalma <nds.suscript@gmail.com>
2024-02-07 14:37:02 -0300

    Ajustes en personas y sedes

--- base/actualizar.py
+++ base/actualizar.py
@@ -8,46 +8,39 @@ from time import gmtime, strftime
 from shutil import rmtree
 import json
 import yaml
-
 from jinja2 import Environment, FileSystemLoader
-
 from markdown import Markdown
 from markdown.inlinepatterns import SimpleTagInlineProcessor
 from markdown.extensions import Extension
 
-markdown_extensiones = [ 'tables', 'attr_list', 'toc', 'abbr', 'footnotes' ]
-markdown_extensiones_config = { 'tables': {}, 'attr_list': {}, 'toc': {}, 'abbr': {}, 'footnotes': {}}
-
-
-# funciones generales
+# ------ funciones generales
 def md_a_html(texto):
     global dat_cfg
     global dat_rec
     global env_jinja2
-
     tpl = env_jinja2.from_string(texto)
     texto = tpl.render(cfg=dat_cfg, rec=dat_rec)
-
     md = Markdown(extensions=markdown_extensiones, extension_configs=markdown_extensiones_config)
     return md.convert(texto)
 
-
 def url_dominio(texto):
     return texto.split(':')[1].strip('/')
-
 
 def leer_yml(ar):
     with open(ar, 'r') as f:
         return yaml.safe_load(f)
 
-
 def borrar_contenido(ruta):
     for sr in listdir(ruta):
         rmtree(f'{ruta}{sr}')
 
+
+# ------ configuracion general
+markdown_extensiones = [ 'tables', 'attr_list', 'toc', 'abbr', 'footnotes' ]
+markdown_extensiones_config = { 'tables': {}, 'attr_list': {}, 'toc': {}, 'abbr': {}, 'footnotes': {}}
+
 ruta_public = '../docs/'
 
-# actualización de configuración
 ar_cfg_in = 'datos/configuracion.yml'
 ar_cfg_out = f'{ruta_public}dat/cfg.js'
 
@@ -56,11 +49,11 @@ with open(ar_cfg_in, 'r') as f:
     with open(ar_cfg_out, 'w') as g:
         g.write('const cfg = ' + json.dumps(dat_cfg))
 
-# compilaciones de recursos
+# ------ compilaciones de recursos
 ar_rec_out = f'{ruta_public}dat/rec.js'
 
 rutas_recursos = [
-    './datos/artistas/',
+    './datos/artistas_invitados/',
     './datos/eventos/',
     './datos/obras/',
     './datos/sedes/'
@@ -74,31 +67,73 @@ with open(ar_rec_out, 'w') as g:
     g.write('const rec = ' + json.dumps(dat_rec))
 
 
-# completado de plantillas de páginas
+# ------  completado de plantillas
 fl = FileSystemLoader('plantillas')
 env_jinja2 = Environment(loader=fl)
 
 env_jinja2.globals.update(markdown = md_a_html)
 env_jinja2.globals.update(url_dominio = url_dominio)
 
-ruta_paginas_in = './datos/paginas/'
 
-borrar_contenido(f'{ruta_public}pag/')
+# completado de plantillas para PERSONAS
+ruta_in = './datos/artistas_invitados/'
+ruta_out = f'{ruta_public}/per/'
+borrar_contenido(ruta_out)
 
-for ar in [a for a in listdir(ruta_paginas_in) if a.endswith('.yml')]:
-    dat_pag = leer_yml(f'{ruta_paginas_in}{ar}')
-    ruta_out = ruta_public + dat_pag['ruta_static']
-    makedirs(dirname(ruta_out), exist_ok=True)
-
+for ar in [a for a in listdir(ruta_in) if a.endswith('.yml')]:
     dat_cfg['actualizacion'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    dat_pag = leer_yml(f'{ruta_in}{ar}')
+    dat_pag['titulo'] = dat_pag['nombre'] + ' ' + dat_pag['apellido']
+
+
+    nom_ar = splitext(ar)[0]
+    arc_out = f'{ruta_out}/{nom_ar}/index.html'
+    makedirs(dirname(arc_out), exist_ok=True)
+
+    tpl = env_jinja2.get_template('persona.html')
+    html = tpl.render(cfg=dat_cfg, pag=dat_pag, rec=dat_rec)
+
+    with open(arc_out, 'w') as f:
+        f.write(html)
+
+# completado de plantillas para SEDESa
+ruta_in = './datos/sedes/'
+ruta_out = f'{ruta_public}sds/'
+borrar_contenido(ruta_out)
+
+for ar in [a for a in listdir(ruta_in) if a.endswith('.yml')]:
+    dat_cfg['actualizacion'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    dat_pag = leer_yml(f'{ruta_in}{ar}')
+    dat_pag['titulo'] = 'Sede - ' + dat_pag['nombre']
+
+    nom_ar = splitext(ar)[0]
+    arc_out = f'{ruta_out}/{nom_ar}/index.html'
+    makedirs(dirname(arc_out), exist_ok=True)
+
+    tpl = env_jinja2.get_template('sede.html')
+    html = tpl.render(cfg=dat_cfg, pag=dat_pag, rec=dat_rec)
+
+    with open(arc_out, 'w') as f:
+        f.write(html)
+
+
+# completado de plantillas de PÁGINAS
+ruta_in = './datos/paginas/'
+ruta_out = f'{ruta_public}pag/'
+borrar_contenido(ruta_out)
+
+for ar in [a for a in listdir(ruta_in) if a.endswith('.yml')]:
+    dat_cfg['actualizacion'] = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    dat_pag = leer_yml(f'{ruta_in}{ar}')
+
+    arc_out = ruta_public + dat_pag['ruta_static']
+    makedirs(dirname(arc_out), exist_ok=True)
 
     tpl = env_jinja2.get_template(dat_pag['html_base'])
     html = tpl.render(cfg=dat_cfg, pag=dat_pag, rec=dat_rec)
 
-    with open(ruta_out, 'w') as f:
+    with open(arc_out, 'w') as f:
         f.write(html)
-
-
 
 # finalización
 print('--- Actualización completada!')
--- base/agregar.py
+++ base/agregar.py
@@ -6,6 +6,12 @@ from sys import argv
 
 def normalizar(t):
     rr = [
+        ['á', 'a'],
+        ['é', 'e'],
+        ['í', 'i'],
+        ['ó', 'o'],
+        ['ú', 'u'],
+        ['ñ', 'n'],
         [' ', '_'],
         ['.', '_'],
         ['__', '_'],
@@ -37,3 +43,21 @@ if argv[1] == 'artista':
             fo.write(pla)
 
     print('Artista agregado!')
+elif argv[1] == 'sede':
+    ree = {
+        '{numero_orden}': argv[2] if len(argv) >= 3 else '',
+        '{nombre}': argv[3] if len(argv) >= 4 else '',
+        }
+    ar_pla = './plantillas/sede.yml'
+    ruta = './datos/sedes/'
+    nom = normalizar(ree['{numero_orden}']) + '_' + normalizar(ree['{nombre}']) + '.yml'
+    ar_yml = f'{ruta}{nom}'
+
+    with open(ar_pla, 'r') as fi:
+        pla = fi.read()
+        for k, v in ree.items():
+            pla = pla.replace(k, v)
+        with open(ar_yml, 'w') as fo:
+            fo.write(pla)
+
+    print('Sede agregada!')
