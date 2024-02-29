ee2252e4f37995958e8bc5c9e80966ddb4204a97
caalma <nds.suscript@gmail.com>
2024-02-29 17:23:02 -0300

    Control de traducción automática - ajuste.

--- docs/lib/js/init-base.js
+++ docs/lib/js/init-base.js
@@ -1,15 +1,12 @@
 function control_pagina_traducida(){
     let xtb = document.querySelector('[http-equiv="X-Translated-By"]'),
-        xtt = document.querySelector('[http-equiv="X-Translated-To"]'),
         vtb = '',
-        vtx = '',
         est = false;
     if(xtb){
         vtb = xtb.attributes['content'].value;
-        vtx = xtt.attributes['content'].value;
         est = true;
     }
-    return [est, vtb, vtx];
+    return [est, vtb];
 }
 
 window.onload = () => {
@@ -17,6 +14,5 @@ window.onload = () => {
     if(tr[0]){
         document.querySelector('.alerta-traduccion').classList.add('traducida');
         document.querySelector('#tr-by').innerHTML = tr[1];
-        document.querySelector('#tr-to').innerHTML = tr[2];
     }
 }
